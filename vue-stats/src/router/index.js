da630bef0f23920d06883434423ae6370b223c17
韦粤信 <283940802@qq.com>
2019-04-24 22:41:12 +0800

    feat: change router & install vue-meta

--- vue-stats/src/router/index.js
+++ vue-stats/src/router/index.js
@@ -1,15 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Meta from 'vue-meta'
+import StartPage from '@/pages/StartPage'
 import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
 export default new Router({
     routes: [{
-        path: '/',
-        name: 'HelloWorld',
-        component: HelloWorld
-    }],
+            path: '/',
+            name: 'Start',
+            component: StartPage
+        },
+        {
+            path: '/:package',
+            name: 'Package',
+            component: StartPage
+        }
+    ],
     mode: 'history'
 
 })
